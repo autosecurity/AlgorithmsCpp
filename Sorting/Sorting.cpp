0b1e8dbe8918d7d03e474fb243a05324188e684d
Shawn Hou <hou_shuo@outlook.com>
2019-06-15 16:56:05 +0800

    创建工程。

---
+++ Sorting/Sorting.cpp
@@ -0,0 +1,13 @@
+// Sorting.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+	std::string desc = "Sorting algoritms begin here...";
+    std::cout << desc << std::endl;
+}
